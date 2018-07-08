f4fadf858bc0740af6f28fc44a4ef486e3fc3d4f
wangfuguitc <516165998@qq.com>
2018-07-08 20:45:24 +0800

    master

--- core/autorun.py
+++ core/autorun.py
@@ -2,17 +2,23 @@ import os
 import time
 import subprocess
 
-BasePath = os.path.abspath('..')
+BasePath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ReportPath = os.path.join(BasePath, 'report')
 
 
 def run_case(case_list, report_dir):
+    result_list = []
     for case in case_list:
         cmd = 'python ' + '"' + case + '"' + ' >> ' + '"' + \
               os.path.join(report_dir, os.path.basename(case).replace('py', 'txt')) + '"'
         sub = subprocess.Popen(cmd, shell=True)
         sub.wait()
-        print(sub.returncode)
+        result_list.append(sub.returncode)
+    return result_list
+
+
+def html_report(case_list, result_list, report_dir):
+    pass
 
 
 if __name__ == '__main__':
@@ -26,4 +32,5 @@ if __name__ == '__main__':
             case_list.append(file_path)
         elif os.path.isdir(file_path):
             dir_list.append(file_path)
-    run_case(case_list, report_dir)
+    result_list = run_case(case_list, report_dir)
+    html_report(case_list, result_list, report_dir)
